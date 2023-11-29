a74b1169878506e84188ac6c7781e20859f2ea6f
PepisMoreno <josefinarmoreno@gmail.com>
2023-11-29 01:04:31 -0300

    agregadas algunas cosas-funciona por ahora

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
 #request sirve para recolectar la data que viene del formulario
+#jsonify para convertir archivos a json y viceversa.
+from flask_cors import CORS
+#cors: recursos de origen cruzado
+from sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+CORS(app)
+#esto supuestamente hace de nexo entre front y back
 
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
+#ver cómo hacer para que app.py también te pueda derivar a la html agregador_plantas_desde_usuario.html
