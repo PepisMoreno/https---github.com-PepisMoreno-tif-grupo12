9efabf4c2962d7369f619cdd8a7b50ba44299c9c
PepisMoreno <josefinarmoreno@gmail.com>
2023-11-29 00:00:24 -0300

    hasta ahí sigue pudiendo mostrar http

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, request
+#request sirve para recolectar la data que viene del formulario
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p> Hello, World </p>"
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
