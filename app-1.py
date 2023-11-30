28bdd82fc8c8e5070b95c1df5a3afab085049d4f
Spizzamus <sofaina1965@gmail.com>
2023-11-30 18:57:53 -0300

    18:57 comentarios y avance. Aún no logro conectar mysql

--- app-1.py
+++ app-1.py
@@ -20,28 +20,26 @@ def index():
     return render_template("index.html")
 
 #-----------LLAMADO DE SQL-----------
-def conexion():
-    return pymysql.connect(host="localhost",
-                           user="root",
-                           password="root",
-                           db="plantas_bd")
 
-cursor=conexion.cursor() 
+conn =pymysql.connect(host="DESKTOP-06JL5EQ",
+                           user="root",
+                           password="salas887",
+                           db="plantas_bd")
+cursor=conn.cursor() 
 
 try:
     with open ("plantas_bd.sql","r") as plantas_bd_sql:
         consulta_sql=plantas_bd_sql.read()
     cursor.execute(consulta_sql)
-    conexion.commit()
+    conn.commit()
 
     
-
-    print("consulta exitosa")
+    print("consulta exitosa!")
 except Exception as e:
-    print(f"Error: {e} .ntente buscar de nuevo.")
+    print(f"Error: {e}. Intente buscar de nuevo.")
 finally:
     cursor.close()
-    conexion.close()
+    conn.close()
 
 
 #creamos otro decorador, para q el usuario ingrese info a la BD
@@ -53,24 +51,40 @@ def agregador(): #en esta función vamos a recibir la info que recolectamos en e
 @app.route("/store", methods=["GET"]) 
 def obtener_plantas():
     try:
-        cursor=conexion.cursor(dictionary=True)
-        cursor.execute("SELECT FROM plantas")
+        conn= pymysql.connect(host="localhost",
+                           user="root",
+                           password="root",
+                           db="plantas_bd")
+        
+        cursor=conn.cursor(dictionary=True)
+        cursor.execute("SELECT*FROM plantas")
         resultados=cursor.fetchall()
     
         return jsonify({'plantas':resultados})
 
     except Exception as e:
-        return jsonify({'error':{e}})
-
+        return jsonify({'error':str(e)})
+    
+    finally:
+        cursor.close()
+        conn.close()
 
 #Debe haber 2: uno que me muestre todos y uno por ID
 @app.route("/store/<int:id>", methods=["GET"])
 def obtener_planta_id(id):
     
     try:
-        cursor=conexion.cursor(dictionary=True)
-        consulta_sql= "SELECT FROM plantas WHERE id=%s"
-        cursor.execute(consulta_sql,(id))
+        """conn= pymysql.connect(host="localhost",
+                           user="root",
+                           password="root",
+                           db="plantas_bd")"""
+        conn =pymysql.connect(host="DESKTOP-06JL5EQ",
+                           user="root",
+                           password="salas887",
+                           db="plantas_bd")
+        cursor=conn.cursor(dictionary=True)
+        consulta_sql= "SELECT * FROM plantas WHERE id=%s"
+        cursor.execute(consulta_sql,(id,))
         
         resultado=cursor.fetchone()
 
@@ -80,26 +94,30 @@ def obtener_planta_id(id):
             return jsonify({'No encontramos la planta que indicaste. Agregala!'})
 
     except Exception as e:
-        return jsonify({'Error:':{e}})
+        return jsonify({'Error':{e}})
 
 #-----------------POST-CREAR-------------------------
 @app.route("/store", methods=["POST"]) 
 def store():
     #print(request.form). request.form contiene el json que envió el cliente
-    nombrePlanta = request.form('nom')
-    nombrecient = request.form('nomcient')
-    fotoplanta = request.files('nomfoto')
-#Guardamos la imagen en el servidor
+    nombrePlanta = request.form['nom']
+    nombrecient = request.form['nomcient']
+    fotoplanta = request.files['nomfoto']
+
+    #Guardamos la imagen en el servidor
     filename = secure_filename(fotoplanta.filename)
     fotoplanta.save(filename)
 
     #Conexión a la base de datos
-    conn= pymysql.connect()
+    conn= pymysql.connect(host="localhost",
+                           user="root",
+                           password="root",
+                           db="plantas_bd")
     cursor= conn.cursor()
 
     #Insertamos datos a la bd
     sql= "INSERT INTO plantas (nombre_comun, nombre_científico, foto) VALUES (%s, %s, %s)";
-    datos=(nombrePlanta,nombrecient,fotoplanta.filename)
+    datos=(nombrePlanta, nombrecient, fotoplanta.filename)
     
     cursor.execute(sql, datos)
     conn.commit()
@@ -124,16 +142,19 @@ def actualizar_planta(id):
     if request.is_json:
         data=request.json
 
-        nombrePlanta = data.get['nom']
-        nombrecient = data.get['nomcient']
-        fotoplanta = data.get['nomfoto']
+        nombrePlanta = data.get('nom')
+        nombrecient = data.get('nomcient')
+        fotoplanta = data.get('nomfoto')
 
         #Conexión a la base de datos
-        conn= pymysql.connect()
+        conn= pymysql.connect(host="localhost",
+                           user="root",
+                           password="root",
+                           db="plantas_bd")
         cursor= conn.cursor()
 
         #Insertamos datos a la bd
-        sql= "INSERT INTO plantas (nombre_comun, nombre_científico, foto, id) VALUES (%s, %s, %s,%s)"
+        sql = "UPDATE plantas SET nombre_comun=%s, nombre_científico=%s, foto=%s WHERE id=%s"
         datos=(nombrePlanta,nombrecient,fotoplanta.filename, id)
         
         cursor.execute(sql, datos)
@@ -144,16 +165,30 @@ def actualizar_planta(id):
 
         return render_template("index.html")
     else:
-        return "La solicitud no contiene datos JSON", 400    
+        return "La solicitud no contiene datos JSON"   
 
 #--------------------DELETE-ELIMINAR------------------------
 @app.route("/delete/<int:id>", methods=["DELETE"])
-def store(id):
-    nombrePlanta = request.form['nom']
-    nombrecient = request.form['nomcient']
-    fotoplanta = request.files*['nomfoto']
-    
+def eliminar_planta(id):
+    try:
+        conn = pymysql.connect(host="localhost",
+                           user="root",
+                           password="root",
+                           db="plantas_bd")
+        cursor = conn.cursor()
 
+        # Eliminar datos en la BD
+        sql = "DELETE FROM plantas WHERE id=%s"
+        cursor.execute(sql, (id,))
+        conn.commit()
+
+        return render_template("index.html")
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    finally:
+        cursor.close()
+        conn.close()
 
 #----------------Programa ppal -----------------------
 if __name__=="__main__":
